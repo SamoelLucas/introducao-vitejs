a9c443b0bff0568f4d3134525efacc06e09a4374
Samoel <samoellucas979@gmail.com>
2026-03-23 10:57:23 -0300

    s

---
+++ src/pages/atividade/atividade-01/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router";
+
+import styles from './index.module.css';
+
+function atividade01() {
+    return (
+        <div className={styles.container}>
+            <h1>Atividade 1</h1>
+            <p>Atividade individual CSS</p>
+        </div>
+    )
+}
+
+export default atividade01;
